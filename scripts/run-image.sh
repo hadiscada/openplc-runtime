b0c848805f0d3a58e95314313c69babcdc771ca3
marcone tenorio <marconetsf@gmail.com>
2025-10-30 15:10:59 +0100

    [RTOP-93] bugfix users db permission

--- scripts/run-image.sh
+++ scripts/run-image.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 # Run container mounting only source code (preserves built venv)
+# Named volume for persistent runtime data (DB, .env, etc)
 docker run --rm -it \
     -v $(pwd)/core:/workdir/core \
     -v $(pwd)/webserver:/workdir/webserver \
     -v $(pwd)/scripts:/workdir/scripts \
+    -v openplc-runtime-data:/var/run/runtime \
     --cap-add=sys_nice \
     --ulimit rtprio=99 \
     --ulimit memlock=-1 \
